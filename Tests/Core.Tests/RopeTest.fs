fb79f447736419a5a95b2efe8b66f3bdb9057d07
Collin McKinley <bravotic@aaathats3as.com>
2025-05-20 15:37:08 -0400

    Added more tests for rope

--- Tests/Core.Tests/RopeTest.fs
+++ Tests/Core.Tests/RopeTest.fs
@@ -24,6 +24,38 @@ let balancedTree2 =
             Rope.Value "c",
             Rope.Value "d"))
 
+//     X
+//    / \
+//   Y   b
+//  / \
+// c   a 
+let balancedTree3 =
+    Rope.Node (2, 1,
+        Rope.Node (1, 1,
+            Rope.Value "c",
+            Rope.Value "a"),
+        Rope.Value "b")
+
+//   X
+//  / \
+// a   Y
+//    / \
+//   b   c
+let balancedTree4 =
+    Rope.Node (1, 2,
+        Rope.Value "a",
+        Rope.Node (1, 1,
+            Rope.Value "b",
+            Rope.Value "c"))
+
+//   X
+//  / \
+// c   d
+let balancedTree5 =
+    Rope.Node (1, 1,
+        Rope.Value "c",
+        Rope.Value "d")
+        
 //   X
 //  / \
 // a   Y
@@ -137,6 +169,14 @@ let ``Single Value is returned when Value is balanced`` () =
     Assert.Equal(Rope.Value "a", Rope.balance (Rope.Value "a"))
 
 [<Fact>]
+let ``Balanced Tree 1 is unchanged when balance is called`` () =
+    Assert.Equal(balancedTree1, Rope.balance balancedTree1)
+
+[<Fact>]
+let ``Balanced Tree 2 is unchanged when balance is called`` () =
+    Assert.Equal(balancedTree2, Rope.balance balancedTree2)
+
+[<Fact>]
 let ``Unbalanced Tree 1 is balanced into Balanced Tree 2`` () =
     Assert.Equal(balancedTree2, Rope.balance unbalancedTree1)
 
@@ -151,3 +191,71 @@ let ``Unbalanced Tree 3 is balanced into Balanced Tree 2`` () =
 [<Fact>]
 let ``Unbalanced Tree 4 is balanced into Balanced Tree 2`` () =
     Assert.Equal(balancedTree2, Rope.balance unbalancedTree4)
+
+////////////////////////////////////////////////////////////////////////////////
+// insert tests
+////////////////////////////////////////////////////////////////////////////////
+
+[<Fact>]
+let ``Empty tree is replaced with single value on insert`` () =
+    Assert.Equal(Rope.Value "a", Rope.insert Rope.Empty 1 "a")
+
+[<Fact>]
+let ``Value is replaced with Node if another value is inserted`` () =
+    Assert.Equal(Rope.Node (1, 1, Rope.Value "a", Rope.Value "b"),
+        Rope.insert (Rope.Value "a") 1 "b")
+
+[<Fact>]
+let ``Value is added to left of Node when pos is < 1`` () =
+    Assert.Equal(Rope.Node (1, 1, Rope.Value "b", Rope.Value "a"),
+        Rope.insert (Rope.Value "a") 0 "b")
+
+[<Fact>]
+let ``Value is added to right of Node when pos is >= 1`` () =
+    Assert.Equal(Rope.Node (1, 1, Rope.Value "a", Rope.Value "b"),
+       Rope.insert (Rope.Value "a") 1 "b")
+
+[<Fact>]
+let ``Value descends left in a Node when pos < left size`` () =
+    Assert.Equal(balancedTree3, Rope.insert balancedTree1 0 "c")
+
+[<Fact>]
+let ``Value descends right in a Node when pos >= left size`` () =
+    Assert.Equal(balancedTree4, Rope.insert balancedTree1 3 "c")
+
+[<Fact>]
+let ``When a left insert would cause an imbalance, balance is called`` () =
+    Assert.Equal(Rope.balance unbalancedTree4,
+        Rope.insert (Rope.insert balancedTree5 0 "b") 0 "a")
+    
+[<Fact>]
+let ``When a right insert would cause an imbalance, balance is called`` () =
+    Assert.Equal(Rope.balance unbalancedTree1, Rope.insert balancedTree4 4 "d")
+
+////////////////////////////////////////////////////////////////////////////////
+// remove tests
+////////////////////////////////////////////////////////////////////////////////
+
+[<Fact>]
+let ``Remove on an Empty tree retuns Empty`` () =
+    Assert.Equal(Rope.Empty, Rope.remove Rope.Empty 0)
+
+[<Fact>]
+let ``Remove on a single Value returns Empty`` () =
+    Assert.Equal(Rope.Empty, Rope.remove (Rope.Value "a") 0)
+
+[<Fact>]
+let ``Remove with a pos >= 1 removes right node and returns left`` () =
+    Assert.Equal(Rope.Value "a", Rope.remove balancedTree1 1)
+
+[<Fact>]
+let ``Remove with a pos of 0 removes left node and returns right`` () =
+    Assert.Equal(Rope.Value "b", Rope.remove balancedTree1 0)
+
+[<Fact>]
+let ``Remove on a node descends right when pos >= left size`` () =
+    Assert.Equal(balancedTree1, Rope.remove balancedTree4 3)
+
+[<Fact>]
+let ``Remove on a node descends left when pos < left size`` () =
+    Assert.Equal(balancedTree1, Rope.remove balancedTree3 0)
