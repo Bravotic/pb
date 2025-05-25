2a940a373d52d530f258a2c51f335d032ca9b7d6
Collin McKinley <bravotic@aaathats3as.com>
2025-05-25 14:58:04 -0400

    Add functions to Rope to accept lists of values
    This allows multiple values to be
    added/removed/listed at once

--- Core/Rope.fs
+++ Core/Rope.fs
@@ -59,6 +59,15 @@ let rec insert r pos value =
             -> Node (1, 1, Value value, Value oldValue)
         | _ -> Value value
 
+// int -> string list -> Rope -> Rope
+// Inserts all the values given into a rope at the specified position.
+let rec insertAll (pos : int) (values : string list) (r : Rope) =
+    match values with
+        | first :: rest ->
+            insert r pos first
+            |> insertAll (startLine + 1) rest
+        | [] -> r
+
 // Rope -> string -> Rope
 // Inserts a value into a Rope at the final position.
 let rec insertEnd r value =
@@ -86,6 +95,15 @@ let rec remove r pos =
         | Value _ -> Empty
         | _ -> Empty
 
+// int -> int -> Rope -> Rope
+// Removes 'count' items from the given rope starting at 'pos'.
+let rec removeAll (pos : int) (count : int) (r : Rope) =
+    if count >= 0 then
+        Rope.remove r pos
+        |> removeAll pos (count - 1)
+    else
+        r
+
 // Rope -> int -> string -> Rope
 // Changes the value at the position specified in the rope to a new value provided.
 let rec change (r : Rope) (pos : int) (newValue : string) =
@@ -108,6 +126,22 @@ let rec get (pos : int) (r : Rope) =
         | Value v ->
             v
         | _ -> ""
+
+// int -> int -> Rope -> string list -> string list
+// Gets all values from the start position to the end position and returns a list of those
+// values.
+let rec private getAllHelper (start : int) (ending : int) (r : Rope) (acc : string list) =
+    if ending < start then
+        acc
+    else
+        get ending r :: acc
+        |> getAllHelper start (ending - 1) r
+
+// int -> int -> Rope -> string list
+// Gets all values from the start position to the end position and returns a list of those
+// values.
+let getAll (start : int) (ending : int) (r : Rope) =
+    getAllHelper start ending r []
 
 // Rope -> string list -> string list
 let rec private toListHelper (r : Rope) (accumulator : string list) =
