554479d33acf0b81546b6fd915b818cf24460742
Collin McKinley <bravotic@aaathats3as.com>
2025-05-27 11:19:58 -0400

    Massively refactor Document API

--- Core/Document.fs
+++ Core/Document.fs
@@ -28,32 +28,92 @@ let toString (lineEnding : string) (doc : Document) =
 let private updateDocument (r : Rope.Rope) =
     { Data = r ; IsModified = true ; Length = Rope.size r }
 
-let private validateParameters fn (line : LineNum) (value : string) (doc : Document) =
-    if line > doc.Length then
-        Error "Line number is not in the document"
-    elif line = 0 && doc.Length <> 0 then
-        Error "Line number can only be zero if document length is zero"
-    else
-        Ok (fn line value doc)
-
-// LineNum -> string -> Document -> Document
-// Inserts the given value before the line number in the document.
-let private insertUnsafe (line : LineNum) (value : string) (doc : Document) =
-    Rope.insert doc.Data (line - 1) value |> updateDocument
-
-// LineNum -> String -> Document -> Result<Document>
-// Calls insertUnsafe while validating parameters given.
-let insert =
-    insertUnsafe
-    |> validateParameters
+// string list -> LineNum -> LineNum -> Document -> Document
+// Inserts all values given into the document at the specified start line. The ending line is ignored here.
+let private insertUnsafe (values : string list) (startLine : LineNum) (endLine : LineNum) (doc : Document) =
+    let clampedStartLine = max 0 (startLine - 1)
     
-// LineNum -> string -> Document -> Document
-// Appends the given value after hte line number in the document.
-let appendUnsafe (line : LineNum) (value : string) (doc : Document) =
-    Rope.insert doc.Data line value |> updateDocument
+    Rope.insertAll clampedStartLine values doc.Data
+    |> updateDocument
 
+// string list -> LineNum -> LineNum -> Document -> Document
+// Appends all values given after the start line specified. The end line is also ignored here.
+let private appendUnsafe (values : string list) (startLine : LineNum) (endLine : LineNum) (doc : Document) =
+    Rope.insertAll startLine values doc.Data
+    |> updateDocument
+
+// int -> int > Document -> Document
+// Removes the range from startLine to endLine inclusive from the document and returns the
+// updated Document.
+let private removeUnsafe (startLine : LineNum) (endLine : LineNum) (doc : Document) =
+    let clampedStartLine = max 0 (startLine - 1)
+    let count = endLine - startLine
+    
+    Rope.removeAll clampedStartLine count doc.Data
+    |> updateDocument
+
+// int -> int -> Document -> (string list, Document)
+// Gets the values from startLine to endLine inclusive and collects them into a string list.
+// Returns a tuple of the resulting string liste along side the Document provided which is
+// unchanged.
+let private listUnsafe (startLine : LineNum) (endLine : LineNum) (doc : Document) =
+    ((Rope.getAll (startLine - 1) (endLine - 1) doc.Data), doc)
+    
+////////////////////////////////////////////////////////////////////////////////
+// The following code is backend agnostic, meaning if Rope was removed as the
+// data source, the following lines would not need to be updated.
+
+let private validateParameters (startLine : LineNum) (endLine : LineNum) (doc : Document) fn =
+    if startLine > endLine then
+        Error "Starting line cannot be larger than ending line"
+    elif startLine <= 0 then
+        Error "Starting line must be within the document"
+    elif endLine > doc.Length then
+        Error "Ending line must be within the document"
+    else
+        Ok (fn startLine endLine doc)
+
+let private validateParametersInsApp startLine endLine doc fn =
+    if startLine = 0 && doc.Length = 0 then
+        Ok (fn startLine endLine doc)
+    else
+        validateParameters startLine endLine doc fn
+
+// LineNum -> LineNum -> String list -> Document -> Result<Document>
+// Calls insertUnsafe while validating parameters given.
+let insert (startLine : LineNum) (_ : LineNum) (values : string list) (doc : Document) =
+    insertUnsafe values
+    |> validateParametersInsApp startLine startLine doc
+   
 // LineNum -> string -> Document -> Result<Document>
 // Applies appendUnsafe while validating parameters given
-let append =
-    appendUnsafe
-    |> validateParameters
+let append (startLine : LineNum) (_ : LineNum) (values : string list) (doc : Document) =
+    appendUnsafe values 
+    |> validateParametersInsApp startLine startLine doc
+
+// LineNum -> LineNum -> Document -> Result<Document>
+// Applies removeUnsafe while v
+let remove (startLine : LineNum) (endLine : LineNum) (doc : Document) =
+    removeUnsafe
+    |> validateParameters startLine endLine doc
+
+// LineNum -> LineNum -> Document -> Result<(string list, Document)>
+// Lists all lines from startLine to endLine inclusive. 
+let list (startLine : LineNum) (endLine : LineNum) (doc : Document) =
+    listUnsafe
+    |> validateParameters startLine endLine doc
+
+// LineNum -> string list -> Document -> Result<Document>
+// Helper function to perform an insert at a position, however if that position
+// is after the end of the document (for example at Length + 1), the appropriate
+// append command is called instead.
+let private insertWrap position values doc =
+    if position <= doc.Length then
+        insert position position values doc
+    else
+        append doc.Length doc.Length values doc
+
+let change (startLine : LineNum) (endLine : LineNum) (values : string list) (doc : Document) =
+    remove startLine endLine doc
+    |> Result.bind (insertWrap startLine values)
+  
--- Tests/Core.Tests/DocumentTest.fs
+++ Tests/Core.Tests/DocumentTest.fs
@@ -50,7 +50,7 @@ let ``toString on ab document produces document with a and b on two lines`` () =
 [<Fact>]
 let ``Line number 0 is valid on empty document`` =
     emptyDocument
-    >>= insert 0 "a"
+    >>= insert 0 0 [ "a" ]
     |> function
         | Ok s -> Assert.True(true)
         | Error s -> Assert.True(false)
@@ -58,8 +58,8 @@ let ``Line number 0 is valid on empty document`` =
 [<Fact>]
 let ``Line number 0 is not valid on document with contents`` =
     emptyDocument
-    >>= insert 0 "a"
-    >>= insert 0 "b"
+    >>= insert 0 0 [ "a" ]
+    >>= insert 0 0 [ "b" ]
     |> function
         | Ok _ -> fail "Expected an error"
         | Error _ -> Assert.True(true)
@@ -67,8 +67,8 @@ let ``Line number 0 is not valid on document with contents`` =
 [<Fact>]
 let ``Line number <= our document length is accepted`` =
     emptyDocument
-    >>= insert 0 "a"
-    >>= insert 1 "b"
+    >>= insert 0 0 [ "a" ]
+    >>= insert 1 1 [ "b" ]
     |> function
         | Ok _ -> Assert.True(true)
         | Error _ -> Assert.True(false)
@@ -76,7 +76,7 @@ let ``Line number <= our document length is accepted`` =
 [<Fact>]
 let ``Line number > our document length is rejected`` =
     emptyDocument
-    >>= insert 100 "a"
+    >>= insert 100 100 [ "a" ]
     |> function
         | Ok _ -> fail "Expected an error"
         | Error _ -> Assert.True(true)
@@ -88,7 +88,7 @@ let ``Line number > our document length is rejected`` =
 [<Fact>]
 let ``Insert on a blank document gives us just one line`` () =
     emptyDocument
-    >>= insert 0 "a"
+    >>= insert 0 0 [ "a" ]
     >>= toTestString
     |> function
         | Ok s -> Assert.Equal("a", s)
@@ -97,7 +97,7 @@ let ``Insert on a blank document gives us just one line`` () =
 [<Fact>]
 let ``Insert on a line that doesn't exist errors out`` () =
     emptyDocument
-    >>= insert 1 "a"
+    >>= insert 1 1 [ "a" ]
     |> function
         | Ok s -> Assert.True(false)
         | Error s -> Assert.True(true)
@@ -105,18 +105,39 @@ let ``Insert on a line that doesn't exist errors out`` () =
 [<Fact>]
 let ``Insert chained gives us multiple lines`` () =
     emptyDocument
-    >>= insert 0 "c"
-    >>= insert 1 "b"
-    >>= insert 1 "a"
+    >>= insert 0 0 [ "c" ]
+    >>= insert 1 1 [ "b" ]
+    >>= insert 1 1 [ "a" ]
     >>= toTestString
     |> function
         | Ok s -> Assert.Equal("a$b$c", s)
         | Error s -> Assert.True(false, s)
+
+[<Fact>]
+let ``Chained calls are equivalent to one call with multiple values`` () =
+    let chained =
+        emptyDocument
+        >>= insert 0 0 [ "d" ]
+        >>= insert 1 1 [ "c" ]
+        >>= insert 1 1 [ "b" ]
+        >>= insert 1 1 [ "a" ]
+
+    let multipleValues =
+        emptyDocument
+        >>= insert 0 0 [ "a" ; "b" ; "c" ; "d" ]
+
+    match chained with
+        | Ok chainedDoc ->
+            match multipleValues with
+                | Ok multipleDoc ->
+                    Assert.Equal(chainedDoc, multipleDoc)
+                | Error s -> fail s
+        | Error s -> fail s
        
 [<Fact>]
 let ``Insert updates isModified flag`` () =
     emptyDocument
-    >>= insert 0 "a"
+    >>= insert 0 0 [ "a" ]
     |> function
         | Ok doc -> Assert.True(doc.IsModified)
         | Error s -> Assert.True(false, s)
@@ -124,9 +145,7 @@ let ``Insert updates isModified flag`` () =
 [<Fact>]
 let ``Insert updates Length`` () =
     emptyDocument
-    >>= insert 0 "a"
-    >>= insert 1 "b"
-    >>= insert 1 "c"
+    >>= insert 0 0 [ "a" ; "b" ; "c" ]
     |> function
         | Ok doc -> Assert.Equal(3, doc.Length)
         | Error s -> fail s
@@ -138,7 +157,7 @@ let ``Insert updates Length`` () =
 [<Fact>]
 let ``Append on an empty document replaces contents`` () =
     emptyDocument
-    >>= append 0 "a"
+    >>= append 0 0 [ "a" ]
     >>= toTestString
     |> function
         | Ok s -> Assert.Equal("a", s)
@@ -147,9 +166,9 @@ let ``Append on an empty document replaces contents`` () =
 [<Fact>]
 let ``Append in succession creates multiple consecutive lines`` () =
     emptyDocument
-    >>= append 0 "a"
-    >>= append 1 "b"
-    >>= append 2 "c"
+    >>= append 0 0 [ "a" ]
+    >>= append 1 1 [ "b" ]
+    >>= append 2 2 [ "c" ]
     >>= toTestString
     |> function
         | Ok s -> Assert.Equal("a$b$c", s)
@@ -158,16 +177,129 @@ let ``Append in succession creates multiple consecutive lines`` () =
 [<Fact>]
 let ``Append sets IsModified flag`` () =
      emptyDocument
-    >>= append 0 "a"
+    >>= append 0 0 [ "a" ]
     |> function
         | Ok doc -> Assert.True(doc.IsModified)
         | Error s -> fail s
 
 [<Fact>]
     emptyDocument
-    >>= append 0 "a"
-    >>= append 1 "b"
-    >>= append 1 "c"
+    >>= append 0 0 [ "a" ]
+    >>= append 1 1 [ "b" ]
+    >>= append 1 1 [ "c" ]
     |> function
         | Ok doc -> Assert.Equal(3, doc.Length)
+        | Error s -> fail s
+
+////////////////////////////////////////////////////////////////////////////////
+// remove tests
+////////////////////////////////////////////////////////////////////////////////
+
+[<Fact>]
+let ``Remove on one line returns Empty document`` () =
+    emptyDocument
+    >>= append 0 0 [ "a" ]
+    >>= remove 1 1
+    >>= toTestString
+    |> function
+        | Ok s -> Assert.Equal("", s)
+        | Error s -> fail s
+
+[<Fact>]
+let ``Remove removes only one line if requested to do so`` () =
+    emptyDocument
+    >>= append 0 0 [ "a" ; "b" ; "c" ]
+    >>= remove 2 2
+    >>= toTestString
+    |> function
+        | Ok s -> Assert.Equal("a$c", s)
+        | Error s -> fail s
+
+[<Fact>]
+let ``Removing a region is equivalent to chained removes`` () =
+    let chained =
+        emptyDocument
+        >>= append 0 0 [ "a" ; "b" ; "c" ]
+        >>= remove 1 1
+        >>= remove 1 1
+
+    let range =
+        emptyDocument
+        >>= append 0 0 [ "a" ; "b" ; "c" ]
+        >>= remove 1 2
+
+    match chained with
+        | Ok chainedDoc ->
+            match range with
+                | Ok rangeDoc ->
+                    Assert.Equal(chainedDoc, rangeDoc)
+                | Error s -> fail s
+        | Error s -> fail s
+
+////////////////////////////////////////////////////////////////////////////////
+// list tests
+////////////////////////////////////////////////////////////////////////////////
+
+let abcDocument =
+    emptyDocument
+    >>= append 0 0 [ "a" ; "b" ; "c" ]
+
+[<Fact>]
+let ``list returns untouched document`` () =
+    abcDocument
+    >>= list 1 1
+    |> function
+        | Ok (_, doc) ->
+            Assert.Equal(abcDocument, Ok doc)
+        | Error s -> fail s
+            
+
+[<Fact>]
+let ``list displays single line if start = end`` () =
+    emptyDocument
+    >>= append 0 0 [ "a" ; "b" ; "c" ]
+    >>= list 1 1
+    |> function
+        | Ok (strlist, doc) ->
+            Assert.Equal("a", (String.concat "" strlist))
+        | Error s -> fail s
+
+[<Fact>]
+let ``list displays proper range when requirested`` () =
+    abcDocument
+    >>= list 1 2
+    |> function
+        | Ok (strlist, _) ->
+            Assert.Equal("a$b", (String.concat "$" strlist))
+        | Error s -> fail s
+
+////////////////////////////////////////////////////////////////////////////////
+// change tests
+////////////////////////////////////////////////////////////////////////////////
+                                                         
+[<Fact>]
+let ``change changes value from one to another`` () =
+    abcDocument
+    >>= change 1 1 [ "g" ]
+    >>= toTestString
+    |> function
+        | Ok s -> Assert.Equal("g$b$c", s)
+        | Error s -> fail s
+
+[<Fact>]
+let ``change in middle of document works`` () =
+    abcDocument
+    >>= change 2 2 [ "g" ]
+    >>= toTestString
+    |> function
+        | Ok s -> Assert.Equal("a$g$c", s)
+        | Error s -> fail s
+
+[<Fact>]
+let ``change at the end of the document works`` () =
+    abcDocument
+    >>= change 3 3 [ "g" ]
+    >>= toTestString
+    |> function
+        | Ok s -> Assert.Equal("a$b$g", s)
         | Error s -> fail s
